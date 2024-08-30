817c4052d348789a62ac0798eb18205dfcb5b575
joserafa78 <jose.rafael.jimenez.6@gmail.com>
2024-08-30 19:46:33 +0200

    merge con remoto

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 //define("URL", "http://localhost/CursoCookies/");
-//define("URL", "http://localhost:3000/");
-define("URL", "https://animated-carnival-7pq5gvr9xvrcpvxq-3000.app.github.dev/");
+define("URL", "http://localhost:3000/");
+//define("URL", "https://animated-carnival-7pq5gvr9xvrcpvxq-3000.app.github.dev/");
 
 // ________________ SESIÓN_____________________
 session_start(); // Inicia un Sesion.
